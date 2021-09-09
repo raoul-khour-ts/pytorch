5ff4bdea97522dad11cb7e29cb0a026302630b4b
myuan <myuan@fb.com>
2021-09-08 18:45:21 -0700

    [RFC] Modularize functions of parsing bytecode
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61862
    
    
    Modularize functions of parsing bytecode tables so that they can be used as needed in situations other than mobile lite interpreter.
    * The decoupled functions are re-used by current lite interpreter loader.
    * The bytecode can be serialized/deserialized from other formats.
    * The decoupled functions have minimum dependencies on other PyTorch components.
    
    Next:
    Build a driver binary to include the parser and interpreter, but only has necessary dependency on other PyTorch components.
    ghstack-source-id: 137644253
    
    Differential Revision: [D29798382](https://our.internmc.facebook.com/intern/diff/D29798382/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D29798382/)!

--- torch/csrc/jit/serialization/import_export_functions.h
+++ torch/csrc/jit/serialization/import_export_functions.h
@@ -3,10 +3,11 @@
 // Functions that are used in both import and export processes
 namespace torch {
 namespace jit {
-void moduleMethodsTuple(
-    const Module& module,
-    std::vector<c10::IValue>& elements);
-IValue expect_field(IValue tup, const std::string& expected_name, size_t entry);
+using c10::IValue;
+const IValue& expect_field(
+    const IValue& tup,
+    const std::string& expected_name,
+    size_t entry);
 std::string operator_str(
     const std::string& name,
     const std::string& overloadname);
