2dba1531652e9a2c4828318e961217b8521e7a48
Mikhail Zolotukhin <mvz@fb.com>
2021-09-09 09:59:52 -0700

    [TensorExpr] Simplify TE IR before applying any transformations.
    This also exposed several bugs, which are fixed in this PR.
    
    ghstack-source-id: 08be7d320c21851440e9c0eb7a972d7009e18a84
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64717

--- test/cpp/tensorexpr/test_kernel.cpp
+++ test/cpp/tensorexpr/test_kernel.cpp
@@ -210,7 +210,11 @@ TEST_F(Kernel, Huge) {
   TensorExprKernel k(graph);
   std::ostringstream oss;
   oss << *k.getCodeGenStmt();
-  const std::string& verification_pattern = "# CHECK: 4000000000";
+  // The 4000000000 iterations loop will be split into 500000000 x 8 and the
+  // outer loop will be parallel. If LLVM is not present, it will not be split,
+  // and to cover both of these cases we're looking for 00000000ll; in the
+  // output.
+  const std::string& verification_pattern = R"IR(# CHECK: 00000000ll;)IR";
   torch::jit::testing::FileCheck().run(verification_pattern, oss.str());
 }
 
@@ -563,13 +567,11 @@ TEST_F(Kernel, CatWoConditionals) {
 # CHECK: for
 # CHECK-NEXT: for
 # CHECK-NEXT: for
-# CHECK-NEXT: aten_cat
+# CHECK: aten_cat
 # CHECK: for
 # CHECK-NEXT: for
-# CHECK-NEXT: for
-# CHECK-NEXT: aten_cat
+# CHECK: aten_cat
 # CHECK: for
-# CHECK-NEXT: for
 # CHECK-NEXT: for
 # CHECK-NEXT: aten_cat)IR";
   torch::jit::testing::FileCheck().run(verification_pattern, oss.str());
